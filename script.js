855dfebf4e4547b33e1130f5e3fca072b4b89822
Faizul <faizulmd1306@gmail.com>
2025-08-21 20:46:01 +0530

    make the new-book form working

--- script.js
+++ script.js
@@ -7,3 +7,16 @@ function Book() {
 function addBookToLibrary() {
   // take params, create a book then store it in the array
 }
+
+document.addEventListener("DOMContentLoaded", () => {
+  const addNewBook = document.querySelector("#new-book");
+  const inputForm = document.querySelector(".new-book-info");
+  const submitButton = document.querySelector("#submit-btn");
+
+  submitButton.addEventListener("click", function () {
+    inputForm.classList.toggle("active");
+  });
+  addNewBook.addEventListener("click", function () {
+    inputForm.classList.toggle("active");
+  });
+});
